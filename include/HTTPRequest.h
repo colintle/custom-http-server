e45c53197c20902dfd1ba967748eae83cacf05f8
Colin Le <Colin.Le@ucf.edu>
2024-12-31 10:24:26 -0500

    Make use of enum http methods for http request

--- include/HTTPRequest.h
+++ include/HTTPRequest.h
@@ -2,6 +2,7 @@
 #define HTTPREQUEST_H
 
 #include <unordered_map>
+#include <variant>
 #include <string>
 #include <stdio.h>
 
@@ -21,13 +22,13 @@ enum HttpMethods
 class HttpRequest
 {
     private:
-        std::unordered_map<std::string, std::string> request_line_fields;
+        std::unordered_map<std::string, std::variant<HttpMethods, std::string>> request_line_fields;
         std::unordered_map<std::string, std::string> header_fields;
         std::unordered_map<std::string, std::string> body_fields;
 
     public:
         HttpRequest(char* request_string);
-        std::unordered_map<std::string, std::string> get_request_line_fields();
+        std::unordered_map<std::string, std::variant<HttpMethods, std::string>> get_request_line_fields();
         std::unordered_map<std::string, std::string> get_header_fields();
         std::unordered_map<std::string, std::string> get_body_fields();
         ~HttpRequest();
--- src/HTTPRequest.cpp
+++ src/HTTPRequest.cpp
@@ -2,6 +2,50 @@
 
 #include <iostream>
 
+HttpMethods extract_method(char* method)
+{
+    if (strcmp(method, "GET") == 0)
+    {
+        return GET;
+    }
+    else if (strcmp(method, "POST") == 0)
+    {
+        return POST;
+    }
+    else if (strcmp(method, "PUT") == 0)
+    {
+        return PUT;
+    }
+    else if (strcmp(method, "HEAD") == 0)
+    {
+        return HEAD;
+    }
+    else if (strcmp(method, "PATCH") == 0)
+    {
+        return PATCH;
+    }
+    else if (strcmp(method, "DELETE") == 0)
+    {
+        return DELETE;
+    }
+    else if (strcmp(method, "CONNECT") == 0)
+    {
+        return CONNECT;
+    }
+    else if (strcmp(method, "OPTIONS") == 0)
+    {
+        return OPTIONS;
+    }
+    else if (strcmp(method, "TRACE") == 0)
+    {
+        return TRACE;
+    }
+    else
+    {
+        return GET;
+    }
+}
+
 std::unordered_map<std::string, std::string> parse_header_fields(char* header_fields)
 {
     std::unordered_map<std::string, std::string> headers;
@@ -26,11 +70,11 @@ std::unordered_map<std::string, std::string> parse_header_fields(char* header_fi
     return headers;
 }
 
-std::unordered_map<std::string, std::string> parse_request_line_fields(char* request_line_fields)
+std::unordered_map<std::string, std::variant<HttpMethods, std::string>> parse_request_line_fields(char* request_line_fields)
 {
-    std::unordered_map<std::string, std::string> request_line = std::unordered_map<std::string, std::string>();
+    std::unordered_map<std::string, std::variant<HttpMethods, std::string>> request_line = std::unordered_map<std::string, std::variant<HttpMethods, std::string>>();
     char* method = strtok(request_line_fields, " ");
-    request_line["method"] = std::string(method);
+    request_line["method"] = extract_method(method);
 
     char* uri = strtok(NULL, " ");
     request_line["uri"] = std::string(uri);
@@ -98,13 +142,13 @@ HttpRequest::HttpRequest(char* request_string)
     this->request_line_fields = parse_request_line_fields(request_line);
     this->header_fields = parse_header_fields(header_fields);
 
-    if (this->request_line_fields["method"] == "POST")
+    if (std::get<HttpMethods>(this->request_line_fields["method"]) == POST)
     {
         this->body_fields = parse_body_fields(body, this->header_fields["Content-Type"]);
     }
 }
 
-std::unordered_map<std::string, std::string> HttpRequest::get_request_line_fields()
+std::unordered_map<std::string, std::variant<HttpMethods, std::string>> HttpRequest::get_request_line_fields()
 {
     return this->request_line_fields;
 }
